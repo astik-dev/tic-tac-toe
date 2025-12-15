ca55cd8b6729dd67181f9293e592c0f661329a7c
ASTIK <52823963+astik-dev@users.noreply.github.com>
2025-12-15 20:29:41 +0100

    Add game logic

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Square, { type SquareProps } from "../Square/Square";
 import ScoreCard from "../ScoreCard/ScoreCard";
 import classes from "./Game.module.scss";
@@ -11,12 +11,26 @@ const INITIAL_GRID = [
 	null, null, null,
 ];
 
+const WIN_LINES = [
+	[ 0, 1, 2 ], // horizontal
+	[ 3, 4, 5 ],
+	[ 6, 7, 8 ],
+
+	[ 0, 3, 6 ], // vertical
+	[ 1, 4, 7 ],
+	[ 2, 5, 8 ],
+
+	[ 0, 4, 8 ], // diagonal
+	[ 2, 4, 6 ],
+];
+
 function Game() {
 
 	const [isGameActive, setIsGameActive] = useState(false);
 	const [player, setPlayer] = useState<Exclude<SquareProps["value"], null>>("X");
 	const [grid, setGrid] = useState<SquareProps["value"][]>(INITIAL_GRID);
 	const [scores, setScores] = useState({ X: 0, O: 0, draw: 0 });
+	const [winner, setWinner] = useState<SquareProps["value"]>(null);
 
 	const handleSquareClick = (squareIndex: number) => {
 		setGrid(prev => {
@@ -26,6 +40,33 @@ function Game() {
 		});
 		setPlayer(p => p === "X" ? "O" : "X");
 	};
+
+	useEffect(() => {
+
+		let winner: SquareProps["value"] = null;
+
+		for (const line of WIN_LINES) {
+			const lineValues = line.map(index => grid[index]);
+			winner = lineValues.reduce((accumulator, current) => {
+				if (accumulator === current && current !== null) {
+					return accumulator;
+				} else {
+					return null;
+				}
+			});
+			if (winner) break;
+		}
+
+		if (winner) {
+			setWinner(winner);
+			setScores(s => ({ ...s, [winner]: s[winner] + 1 }));
+			setIsGameActive(false);
+		} else if (grid.every(v => v !== null)) {
+			setScores(s => ({ ...s, draw: s.draw + 1 }));
+			setIsGameActive(false);
+		}
+
+	}, [grid]);
 
 	return (
 		<div className={classes.game}>
@@ -60,7 +101,9 @@ function Game() {
 				}
 
 				{!isGameActive && (scores.X + scores.draw + scores.O) > 0 &&
-					<div className={classes.banner}>Game over.</div>
+					<div className={classes.banner}>
+						Game over. {winner ? `Player ${winner} Wins!` : "It's a Draw!"}
+					</div>
 				}
 
 			</div>
@@ -68,7 +111,7 @@ function Game() {
 			{!isGameActive &&
 				<button
 					className={classes.newGameButton}
-					onClick={() => setIsGameActive(true)}
+					onClick={() => { setIsGameActive(true); setGrid(INITIAL_GRID) }}
 				>
 					New Game
 				</button>
