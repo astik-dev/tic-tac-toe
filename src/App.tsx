4b850a3c4eb855d9c3c5816b13f44efc5efcd657
ASTIK <52823963+astik-dev@users.noreply.github.com>
2025-12-16 01:23:52 +0100

    Prevent app render until fonts loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
 import classes from "./App.module.scss";
 import Game from "./components/Game/Game";
 
 function App() {
-	return (
+
+	const [fontsLoaded, setFontsLoaded] = useState(false);
+
+	useEffect(() => {
+		Promise.all([
+			document.fonts.load('16px "Inter"'),
+			document.fonts.load('16px "Fredoka"')
+		]).then(() => setFontsLoaded(true));
+	}, []);
+
+	return fontsLoaded ? (
 		<div className={classes.app}>
 			<h1>tic.<br/><span>tac.</span><br/>toe.</h1>
 			<Game/>
 		</div>
-	);
+	) : null;
 }
 
 export default App;
