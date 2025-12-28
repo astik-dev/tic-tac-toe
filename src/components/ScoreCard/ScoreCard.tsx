b12bf695b5f69bb6e4544c7cbbe29d2c392272a7
ASTIK <52823963+astik-dev@users.noreply.github.com>
2025-12-28 15:49:45 +0100

    Optimize ScoreCard re-renders with React.memo

--- src/components/ScoreCard/ScoreCard.tsx
+++ src/components/ScoreCard/ScoreCard.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import classes from "./ScoreCard.module.scss";
 
 type ScoreCardProps = {
@@ -6,13 +7,13 @@ type ScoreCardProps = {
 	color: string,
 }
 
-function ScoreCard({ label, score, color }: ScoreCardProps) {
+const ScoreCard = memo(function ScoreCard({ label, score, color }: ScoreCardProps) {
 	return (
 		<div className={classes.scoreCard} style={{ background: color }}>
 			<span className={classes.label}>{label}</span>
 			<span className={classes.score}>{score}</span>
 		</div>
 	);
-}
+});
 
 export default ScoreCard;
